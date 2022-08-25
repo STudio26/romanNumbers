edb4b9c38d86eddfc4d41014ae7e0ca585f10318
Alain <github@STudio26.no-ip.biz>
2022-08-25 15:06:59 +0200

    Create project.

---
+++ src/main/java/org/example/RomanNumber.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class RomanNumber {
+    public RomanNumber() {
+    }
+
+    int[] arabicValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] romanValues = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public String convert(int value) {
+        StringBuilder result = new StringBuilder();
+        while (value != 0)
+            for(int i = 0; i < arabicValues.length; i++)
+                if (value >= arabicValues[i]) {
+                    result.append(romanValues[i]);
+                    value -= arabicValues[i];
+                    break;
+                }
+        return result.toString();
+    }
+}
